1f88b2ddc3dba38c113c2efa3c648b117e618f8b
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-10-03 20:00:52 +0000

    Ready!

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -27,20 +27,19 @@ class VenuesController < ApplicationController
   def update
     the_id = params.fetch("the_id")
 
-    @venue = Venue.where({ :id => the_id })
-    @venue.address = params.fetch("query_address")
-    @venue.name = params.fetch("Query_name")
-    @venue.neighborhood = params.fetch("query_neighborhood")
-    @venue.save
+    @the_venue = Venue.where({ :id => the_id }).at(0)
+    @the_venue.address = params.fetch("query_address")
+    @the_venue.name = params.fetch("query_name")
+    @the_venue.neighborhood = params.fetch("query_neighborhood")
+    @the_venue.save
     
-    redirect_to("/venues/#{@venue.id}")
+    redirect_to("/venues/#{@the_venue.id}")
   end
 
   def destroy
     the_id = params.fetch("the_id")
-    matching_venues = Venue.where({ :id => the_id })
-    @venue = matching_venues
-    @venue.destroy
+    @the_venue = Venue.where({ :id => the_id }).at(0)
+    @the_venue.destroy
 
     redirect_to("/venues")
   end
